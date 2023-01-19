2c908fbee5a92d67e2f3414143c0f9a978d1df9f
kobelev9 <kirill97kobelev@gmail.com>
2023-01-18 21:44:43 -0600

    added DocumentsPage class

---
+++ src/utils/DocumentsPage.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class DocumentsPage {
+
+    public static void d(){
+        // creating the document
+
+        // editing the document
+
+        // saving the document
+
+        // deleting the document
+    }
+}
